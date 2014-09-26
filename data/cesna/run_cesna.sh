1f1887906fb4a9b869769f0dba0e0b8e59b241e4
Mackenzie Sweeney <mackenzie.sweeney@gmail.com>
2014-09-26 16:14:03 -0400

    removed old data files

--- data/cesna/run_cesna.sh
+++
@@ -1,6 +0,0 @@
-~/workshop/sna/Snap-2.2/examples/cesna/cesna \
--i:pi-graph-edges.txt \  # the edges of the graph, with ids representing PIs
--a:pi-representative-doc-terms.txt \  # node features (terms in BoW for PI)
--n:abstracts-features.txt -c:96 \  # mappings from term IDs to their terms
--c:96 \  # number of communities to detect (main parameter)
-# -l:node-labels-pi-names.txt  # the names of the PIs (node id/name pairs)
